54e5f6f9156b7f178db8ff88f92dd115e2987018
Ilya Dutlov <id@dutlov.ru>
2022-01-21 00:05:27 +0300

    setup project: 1 and 2 steps

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument(
+        '-f',
+        '--format',
+        action='store',
+        help='set format of output',
+    )
+    args = parser.parse_args()
+
+    #print(generate_diff(args.first_file, args.second_file, args.format))
+
+
+if __name__ == '__main__':
+    main()
