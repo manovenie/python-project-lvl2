5cc84bdacee5b11796708197e7670d07d7c13d9e
Ilya Dutlov <id@dutlov.ru>
2022-01-21 18:40:39 +0300

    fix poetry and plain diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,19 +6,20 @@ def generate_diff(file_path1, file_path2):
     y = json.load(open(file_path2))
     res = '{\n'
     all_keys = sorted(list(x.keys() | y.keys()))
-    deleted_from_file1 = x.keys() - y.keys()
-    added_to_file2 = y.keys() - x.keys()
     intersection_keys = x.keys() & y.keys()
+    deleted_keys = x.keys() - y.keys()
+    added_keys = y.keys() - x.keys()
     for key in all_keys:
-        if key in deleted_from_file1:
-            res += '\t- {}: {}\n'.format(key, x[key])
-        elif key in added_to_file2:
-            res += '\t+ {}: {}\n'.format(key, y[key])
-        elif key in intersection_keys:
+        if key in intersection_keys:
             if x[key] == y[key]:
                 res += '\t  {}: {}\n'.format(key, x[key])
             else:
                 res += '\t- {}: {}\n'.format(key, x[key])
                 res += '\t+ {}: {}\n'.format(key, y[key])
+        elif key in deleted_keys:
+            res += '\t- {}: {}\n'.format(key, x[key])
+        elif key in added_keys:
+            res += '\t+ {}: {}\n'.format(key, y[key])
+
     res += '}\n'
     return res
