4d9e580cdb0853412eed18fc65da8b5159813a97
Ilya Dutlov <id@dutlov.ru>
2022-01-28 01:25:40 +0300

    fix tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -16,8 +16,8 @@ def test_stylish_json():
 def test_stylish_yaml():
     base_path = Path(__file__).parent
     file_path = (base_path / "../tests/fixtures/result_stylish.txt").resolve()
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
+    path1 = 'tests/fixtures/file1.yaml'
+    path2 = 'tests/fixtures/file2.yaml'
     output = generate_diff(path1, path2)
     with open(file_path, 'r') as result:
         assert output == result.read()
@@ -38,8 +38,8 @@ def test_plain_json():
 def test_plain_yaml():
     base_path = Path(__file__).parent
     file_path = (base_path / "../tests/fixtures/result_plain.txt").resolve()
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
+    path1 = 'tests/fixtures/file1.yaml'
+    path2 = 'tests/fixtures/file2.yaml'
     output = generate_diff(path1, path2, 'plain')
     with open(file_path, 'r') as result:
         assert output == result.read()
