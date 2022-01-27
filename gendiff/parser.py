9fa97a5a96f50f045d17456884d9d35ab57e9d71
Ilya Dutlov <id@dutlov.ru>
2022-01-27 19:10:14 +0300

    fix cli parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -8,8 +8,10 @@ def parse_cli_args():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument('-f', '--format', default='stylish', action='store',
-                        help='set format of output')
+    parser.add_argument('-f', '--format', default='stylish',
+                        choices=['stylish', 'plain']
+                        help='set format of output' \
+                            '(default: stylish')
     return parser.parse_args()
 
 
