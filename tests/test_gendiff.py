08421b44ca2d80c00a6ce25ff823ffcb6f9f65f6
Ilya Dutlov <id@dutlov.ru>
2022-01-31 23:51:17 +0300

    add json output to tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -44,3 +44,18 @@ def test_plain_yaml():
     with open(file_path, 'r') as result:
         assert output == result.read()
         assert isinstance(output, str)
+
+
+def test_json():
+    base_path = Path(__file__).parent
+    file_path = (base_path / "../tests/fixtures/result_json.txt").resolve()
+    path1 = 'tests/fixtures/file1.json'
+    path2 = 'tests/fixtures/file2.json'
+    output = generate_diff(path1, path2, 'json')
+    with open(file_path, 'r') as result:
+        assert output == result.read()
+    path1 = 'tests/fixtures/file1.yaml'
+    path2 = 'tests/fixtures/file2.yaml'
+    output = generate_diff(path1, path2, 'json')
+    with open(file_path, 'r') as result:
+        assert output == result.read()
