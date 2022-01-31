a7226cec91bcb97ec59a6bc9e496e22b78223e96
Ilya Dutlov <48948500+dutlov@users.noreply.github.com>
2022-01-31 23:36:30 +0300

    fix lint errors

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -4,6 +4,7 @@ import json
 def format_json(diff):
     return json.dumps(sort_difference(diff))
 
+
 def sort_difference(diff):
     result = {}
     sort_list_keys = sorted(diff.keys())
@@ -11,5 +12,5 @@ def sort_difference(diff):
         result[key] = diff[key]
         if isinstance(diff[key], list):
             if isinstance(diff[key][1], dict):
-                result[key][1] = sorted_diff(diff[key][1])
+                result[key][1] = sort_difference(diff[key][1])
     return result
