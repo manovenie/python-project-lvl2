4a138c253ba5164c6489f708f2717ec9bb82bb91
Ilya Dutlov <id@dutlov.ru>
2022-02-02 02:34:46 +0300

    refactor and shorten code

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,61 +1,22 @@
+import pytest
 from gendiff.gendiff import generate_diff
 from pathlib import Path
 
 
-def test_stylish_json():
+@pytest.mark.parametrize(
+    'style, file_before, file_after, file_result',
+    [
+        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('json', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_json.txt'),  # noqa:E501
+        ('json', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_json.txt'),  # noqa:E501
+    ],
+)
+def test_generate_diff(style, file_before, file_after, file_result):
     base_path = Path(__file__).parent
-    file_path = (base_path / "../tests/fixtures/result_stylish.txt").resolve()
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
-    output = generate_diff(path1, path2)
-    with open(file_path, 'r') as result:
-        assert output == result.read()
-        assert isinstance(output, str)
-
-
-def test_stylish_yaml():
-    base_path = Path(__file__).parent
-    file_path = (base_path / "../tests/fixtures/result_stylish.txt").resolve()
-    path1 = 'tests/fixtures/file1.yaml'
-    path2 = 'tests/fixtures/file2.yaml'
-    output = generate_diff(path1, path2)
-    with open(file_path, 'r') as result:
-        assert output == result.read()
-        assert isinstance(output, str)
-
-
-def test_plain_json():
-    base_path = Path(__file__).parent
-    file_path = (base_path / "../tests/fixtures/result_plain.txt").resolve()
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
-    output = generate_diff(path1, path2, 'plain')
-    with open(file_path, 'r') as result:
-        assert output == result.read()
-        assert isinstance(output, str)
-
-
-def test_plain_yaml():
-    base_path = Path(__file__).parent
-    file_path = (base_path / "../tests/fixtures/result_plain.txt").resolve()
-    path1 = 'tests/fixtures/file1.yaml'
-    path2 = 'tests/fixtures/file2.yaml'
-    output = generate_diff(path1, path2, 'plain')
-    with open(file_path, 'r') as result:
-        assert output == result.read()
-        assert isinstance(output, str)
-
-
-def test_json():
-    base_path = Path(__file__).parent
-    file_path = (base_path / "../tests/fixtures/result_json.txt").resolve()
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
-    output = generate_diff(path1, path2, 'json')
-    with open(file_path, 'r') as result:
-        assert output == result.read()
-    path1 = 'tests/fixtures/file1.yaml'
-    path2 = 'tests/fixtures/file2.yaml'
-    output = generate_diff(path1, path2, 'json')
-    with open(file_path, 'r') as result:
-        assert output == result.read()
+    file_result_path = (base_path / file_result).resolve()
+    with open(file_result_path, 'r') as file:
+        result = file.read()
+    assert generate_diff(file_before, file_after, style) == result
