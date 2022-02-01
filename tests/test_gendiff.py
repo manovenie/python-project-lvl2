156228e1d5fe1a47c29df94d2df1b1f9b09cc6db
Ilya Dutlov <id@dutlov.ru>
2022-02-02 02:43:15 +0300

    refactor and shorten code

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from gendiff.gendiff import generate_diff
 from pathlib import Path
 
@@ -6,17 +7,15 @@ from pathlib import Path
 @pytest.mark.parametrize(
     'style, file_before, file_after, file_result',
     [
-        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_stylish.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_stylish.txt'),  # noqa:E501
-        ('json', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', '../tests/fixtures/result_json.txt'),  # noqa:E501
-        ('json', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '../tests/fixtures/result_json.txt'),  # noqa:E501
+        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('json', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_json.txt'),  # noqa:E501
+        ('json', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_json.txt'),  # noqa:E501
     ],
 )
 def test_generate_diff(style, file_before, file_after, file_result):
-    base_path = Path(__file__).parent
-    file_result_path = (base_path / file_result).resolve()
-    with open(file_result_path, 'r') as file:
-        result = file.read()
+    with open(os.path.abspath(file_result)) as f:
+        result = f.read()
     assert generate_diff(file_before, file_after, style) == result
