361df9fc016eb36c439c5e64eb7fa34ecf31d029
Ilya Dutlov <id@dutlov.ru>
2022-02-02 11:05:36 +0300

    fix error

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -5,11 +5,8 @@ from json.decoder import JSONDecodeError
 from yaml.scanner import ScannerError
 
 
-EXT_ERROR_MSG = '{0} - invalid file extension\nUse one of: .json .yaml .yml'
 YAML_ERROR_MSG = '{0} - incorrect YAML file'
 JSON_ERROR_MSG = '{0} - incorrect JSON file'
-OS_ERROR_MSG = '{0} - file access error'
-INDEX_ERROR_MSG = '{0} - has no file extension'
 
 
 def prepare_file(file_path):
@@ -22,8 +19,6 @@ def prepare_file(file_path):
         raise YAML_ERROR_MSG.format(file_path)
     except JSONDecodeError:
         raise JSON_ERROR_MSG.format(file_path)
-    except OSError:
-        raise OS_ERROR_MSG.format(file_path)
 
 
 def get_format(file_path):
