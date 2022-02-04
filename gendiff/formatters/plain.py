664656c9984f3a14b5c3b35444db473f887e721f
Ilya Dutlov <id@dutlov.ru>
2022-02-05 02:34:09 +0300

    fix code style

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -3,7 +3,6 @@ from gendiff.status_constants import (
     CHANGED,
     DELETED,
     NESTED,
-    UNCHANGED
 )
 
 ADDED_STR = "Property '{0}' was added with value: {1}"
@@ -18,34 +17,28 @@ def format_plain(diff, key_path=None):  # noqa: C901
     for key, value in sorted(diff.items()):
         key_path.append(key)
         status, rest = value[0], value[1:]
-        if status == UNCHANGED:
-            key_path.pop()
-            continue
-        elif status == ADDED:
-            res.append(ADDED_STR.format(
-                '.'.join(key_path), format_value(rest[0])))
-            key_path.pop()
-        elif status == DELETED:
-            res.append(DELETED_STR.format('.'.join(key_path)))
-            key_path.pop()
-        elif status == NESTED:
-            res.append(format_plain(rest[0], key_path))
-            key_path.pop()
-        elif status == CHANGED:
+        if status == CHANGED:
             res.append(CHANGED_STR.format(
                 '.'.join(key_path), format_value(rest[0]),
                 format_value(rest[1])))
-            key_path.pop()
+        elif status == ADDED:
+            res.append(ADDED_STR.format(
+                '.'.join(key_path), format_value(rest[0])))
+        elif status == DELETED:
+            res.append(DELETED_STR.format('.'.join(key_path)))
+        elif status == NESTED:
+            res.append(format_plain(rest[0], key_path))
+        key_path.pop()
     return '\n'.join(res)
 
 
 def format_value(value):
     if type(value) in (list, dict):
         return '[complex value]'
-    elif isinstance(value, bool):
+    if isinstance(value, bool):
         return str(value).lower()
-    elif value is None:
+    if value is None:
         return 'null'
-    elif isinstance(value, str):
+    if isinstance(value, str):
         return f"'{value}'"
     return value
