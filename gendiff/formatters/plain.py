ad65b78168a73d4301063dbd59a09cdb03fd9e50
Ilya Dutlov <id@dutlov.ru>
2022-02-05 23:15:51 +0300

    fix style

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -17,11 +17,9 @@ def format_plain(diff, key_path=None):  # noqa: C901
     for diff_key, diff_value in sorted(diff.items()):
         key_path.append(diff_key)
         status, rest = diff_value[0], diff_value[1:]
-        value = rest[0]
-        formatted_value = format_value(value)
+        formatted_value = format_value(rest[0])
         if status == CHANGED:
-            updated_value = rest[1]
-            formatted_updated_value = format_value(updated_value)
+            formatted_updated_value = format_value(rest[1])
             res.append(CHANGED_STR.format(
                 '.'.join(key_path), formatted_value, formatted_updated_value))
         if status == ADDED:
@@ -30,7 +28,7 @@ def format_plain(diff, key_path=None):  # noqa: C901
         if status == DELETED:
             res.append(DELETED_STR.format('.'.join(key_path)))
         if status == NESTED:
-            res.append(format_plain(value, key_path))
+            res.append(format_plain(rest[0], key_path))
         key_path.pop()
     return '\n'.join(res)
 
