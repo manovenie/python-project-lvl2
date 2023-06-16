e623ddcad2711b9e4cfc30478c4b02044de88127
manovenie <manilovx@gmail.com>
2023-06-16 23:29:00 +0300

    fix len of params

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,12 +6,17 @@ from gendiff.gendiff import generate_diff
 @pytest.mark.parametrize(
     'style, file_before, file_after, file_result',
     [
-        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json',
+         'tests/fixtures/result_plain.txt'),
+        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
+         'tests/fixtures/result_plain.txt'),
+        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json',
+         'tests/fixtures/result_stylish.txt'),
+        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
+         'tests/fixtures/result_stylish.txt'),
     ],
 )
+
 def test_generate_diff(style, file_before, file_after, file_result):
     with open(os.path.abspath(file_result)) as f:
         result = f.read()
